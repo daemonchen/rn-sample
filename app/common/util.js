01971514803d722f9a32b7fe1e10a4271269975e
scott <cgyqqcgy@126.com>
2015-11-27 10:50:10 +0800

    remove useless dependinces & add orderdetail rightbtn config stuff

--- app/common/util.js
+++ app/common/util.js
@@ -38,3 +38,8 @@ exports.parseImgUrl = function (url) {
     }
     return url
 }
+
+exports.getRating = function(rating) {
+    if(rating > 5 || rating < 0) throw new Error('数字不在范围内');
+    return "★★★★★☆☆☆☆☆".substring(5 - rating, 10 - rating );
+}
--- app/views/order/orderDetail.js
+++ app/views/order/orderDetail.js
@@ -8,6 +8,7 @@ var {
     ListView,
     Image,
     Navigator,
+    ActionSheetIOS,
     TouchableOpacity,
     StyleSheet
 } = React
@@ -16,6 +17,7 @@ var commonStyle = require('../../styles/commonStyle');
 
 var Task = require('./task/task');
 var OrderDetailSegmentControl = require('./components/orderDetailSegmentControl');
+var OrderSettings = require('./orderSettings');
 var _navigator, _topNavigator = null;
 
 module.exports = React.createClass({
@@ -33,13 +35,69 @@ module.exports = React.createClass({
                 _navigator.pop()
         }
     },
+    _pressSettingButton: function(){
+        _topNavigator.push({
+            component: OrderSettings,
+            sceneConfig: Navigator.SceneConfigs.FloatFromBottom,
+            topNavigator: _topNavigator
+        });
+    },
+    doCreate: function(index){
+        console.log(index);
+        // switch(index){
+        //     case 0:
+        //         this.setState({
+        //             orderStatus: index
+        //         })
+        //         return this.doPush(OrderSettings);
+        //     case 1:
+        //         this.setState({
+        //             orderStatus: index
+        //         })
+        //         return this.doPush(OrderTemplates);
+        //     case 2:
+        //         this.setState({
+        //             orderStatus: index
+        //         })
+        //         return this.doPush(OrderTemplates);
+        //     default:
+        //         this.setState({
+        //             orderStatus: 0
+        //         })
+        //         return this.doPush(OrderSettings);
+        // }
+    },
+    _pressCreateButton: function(){
+        var self = this;
+        ActionSheetIOS.showActionSheetWithOptions({
+            options: this.actionList,
+            cancelButtonIndex: 3,
+            // destructiveButtonIndex: 1,
+            },
+            (buttonIndex) => {
+                self.doCreate(buttonIndex)
+              // self.setState({ clicked: self.actionList[buttonIndex] });
+            });
+    },
+    actionList: ['新建任务','添加成员','上传附件','取消'],
     rightButtonConfig: function(){
         var self = this;
-        return{
-            title: '+',
-            handler:() =>
-                _navigator.pop()
-        }
+        return(
+            <View style={{width: 72,flexDirection:'row',alignItems:'flex-end'}}>
+                <TouchableOpacity onPress={this._pressCreateButton}>
+                    <Image source={require('../../images/Setting.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this._pressSettingButton}
+                style={{marginLeft:8}}>
+                    <Image source={require('../../images/Setting.png')} />
+                </TouchableOpacity>
+            </View>
+            );
+        // return{
+        //     title: '+',
+        //     handler:() =>
+        //         _navigator.pop()
+        // }
     },
     onPressTaskRow: function(rowData, sectionID){
         console.log('---rowData', rowData);
