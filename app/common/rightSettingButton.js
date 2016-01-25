c9fc7e7d3aa3991dce3cb210c3fc03e2307ff97a
陈光远 <chenguangyuan@chenguangyuandeMacBook.local>
2016-01-25 22:50:22 +0800

    fixed button fontsize

--- app/common/rightSettingButton.js
+++ app/common/rightSettingButton.js
@@ -25,7 +25,7 @@ module.exports = React.createClass({
         return(
             <TouchableOpacity onPress={this.onPress}>
                 <Text
-                style={[{ width: 30, height: 24, marginRight: 16, marginLeft: 16, color: '#4285f4' }, this.props.style]}>
+                style={[{fontSize: 17, width: 36, height: 24, marginTop: 2, marginRight: 16, marginLeft: 16, color: '#4285f4' }, this.props.style]}>
                     设置
                 </Text>
             </TouchableOpacity>
