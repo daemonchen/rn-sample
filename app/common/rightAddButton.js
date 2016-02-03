cc9600da9129e69cff9b27eb3e58217be3a7385b
陈光远 <chenguangyuan@chenguangyuandeMacBook.local>
2016-02-03 17:01:25 +0800

    change right add button UI

--- app/common/rightAddButton.js
+++ app/common/rightAddButton.js
@@ -24,7 +24,7 @@ module.exports = React.createClass({
             return(
             <TouchableOpacity onPress={this.onPress}>
                 <Text
-                style={[{fontSize: 17, width: 36, height: 24, marginTop: 2, marginRight: 8, marginLeft: 8, color: '#4285f4' }, this.props.style]}>
+                style={[{fontSize: 17, width: 36, height: 24, marginTop: 2, marginRight: 16, marginLeft: 8, color: '#4285f4' }, this.props.style]}>
                     {this.props.title}
                 </Text>
             </TouchableOpacity>
